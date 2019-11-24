d104855699f1e036a568213ce7e2e7aff76b1958
Maxim-Babiy <koldmax@ukr.net>
2019-11-24 21:16:03 +0200

    Поправил ДЗ#2

--- dz2/index.php
+++ dz2/index.php
@@ -3,10 +3,15 @@ require('src/functions.php');
 
 $lines = ['Строка №1', 'Строка №2', 'Строка №3', 'Строка №4'];
 $result = task1($lines, false);
-echo $result;
+echo $result . '<br>';
+$result = task1($lines, true);
+echo $result . '<br>';
 echo '<hr>';
 
-echo task2('+',100,5,4,2);
+task2('+',15,5,3,1);
+task2('*',5,4,3,2);
+task2('-',15,5,3,1);
+task2('/',200,10,5,2);
 echo '<hr>';
 
 task3(8,10);
--- dz2/src/functions.php
+++ dz2/src/functions.php
@@ -1,58 +1,66 @@
 <?php
-function task1($param, $a = true) {
-    if ($a != true) {
-        foreach ($param as $value) {
-            echo '<p>' . $value . '</p>';
+function task1($strings, $concat = true) {
+    if ($concat == false) {
+        echo '<b>Если второй параметр = false:</b><br>';
+        foreach ($strings as $string) {
+            echo '<p>' . $string . '</p>';
         }
     } else {
-        $result = '';
-        foreach ($param as $value) {
-           $result = $result . $value . ' ';
-        }
+        echo '<b>Если второй параметр = true:</b><br>';
+        $result = implode(' ', $strings);
         return $result;
     }
 }
 
-function task2(string $operation) {
+function task2(...$arguments) {
     $args = func_get_args();
     $args_numb = func_num_args();
+    $operator = array_shift($arguments);
     $result = 0;
 
-    if ($operation == '+') {
+    if ($operator == '+') {
         for ($i = 1; $i < sizeof($args); $i++) {
             $result += $args[$i];
+            $expression = implode(" $operator ", $arguments);
         }
-    } elseif ($operation == '*') {
+        echo $expression . ' = ' . $result . '<br>';
+    } elseif ($operator == '*') {
         $result = 1;
         for ($i = 1; $i < sizeof($args); $i++) {
             $result *= $args[$i];
+            $expression = implode(" $operator ", $arguments);
         }
-    } elseif ($operation == '-') {
+        echo $expression . ' = ' . $result . '<br>';
+    } elseif ($operator == '-') {
+        $result = $args[1];
         for ($i = 1; $i < sizeof($args); $i++) {
-            $result -= $args[$i];
+            $result -= $args[$i+1];
+            $expression = implode(" $operator ", $arguments);
         }
-    } elseif ($operation == '/') {
+        echo $expression . ' = ' . $result . '<br>';
+    } elseif ($operator == '/') {
         $result = $args[1];
         for ($i = 1; $i <= ($args_numb-2); $i++) {
             $result /= $args[$i+1];
+            $expression = implode(" $operator ", $arguments);
         }
+        echo $expression . ' = ' . $result . '<br>';
     }
-    return $result;
 }
 
 function task3($rows, $cols) {
     if (is_int($rows) && is_int($cols)) {
         echo '<table>';
-        for ($i = 1; $i <= $rows; $i++) {
+        for ($row = 1; $row <= $rows; $row++) {
             echo '<tr>';
-            for ($c = 1; $c <= $cols; $c++) {
-                echo '<td>' . $i * $c . '</td>';
+            for ($col = 1; $col <= $cols; $col++) {
+                echo '<td>' . $row * $col . '</td>';
             }
             echo '</tr>';
         }
         echo '</table>';
     } else {
-        echo '<h2>Текст корректной ошибки :)</h2>';
+        echo '<h2>Пожалуйста, введите целые числа</h2>';
     }
 }
 
@@ -77,8 +85,6 @@ function task6() {
 }
 
 function task7($file_name) {
-    $fp = fopen($file_name, 'w+');
-    fwrite($fp, $string = 'Hello again!');
+    file_put_contents($file_name, 'Hello again!');
     echo file_get_contents($file_name);
-
 }
